469e9af891a1fd63ab864036efe56ba5f3467519
Eduardo <joselincango73@gmail.com>
2025-03-05 00:08:39 -0500

    Pruebas16

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server:{
     host: '0.0.0.0',
-    port: process.env.PORT || 5174, 
-    proxy: {
-      '/api': 'http://localhost:3000', 
-    },
+    port: 'http://localhost:3000',
   },
-  build: {
-    outDir: 'build',
-  }
-});
+})
